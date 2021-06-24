6bb3dbf67ea9f8c52cba0c298d4fdb0a0179f975
rafetr <rafetr@risk.az>
2021-06-24 19:56:01 +0400

    lesson 19

---
+++ Lesson19/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tasks tasks = new Tasks();
+            //Console.WriteLine(tasks.Area(6, 8, 90));
+            //Console.WriteLine(tasks.OlymUsersCount(2, 4, 7, 3));
+        }
+    }
+}
---
+++ Lesson19/Tasks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson19
+{
+    class Tasks
+    {
+        public double Area(double a,double b,double alfa)
+        {
+            //Mathematical : Area = a*b*sin(alfa)/2
+
+            return a * b * Math.Sin(alfa * Math.PI / 180.0) / 2;
+        }
+
+        public int OlymUsersCount(int k, int m, int n, int d)
+        {
+            double x = 1.0 - (double)(k * m + k * n + m * n) / (k * m * n);
+            return (int)(double)(d / x);
+        }
+    }
+}
