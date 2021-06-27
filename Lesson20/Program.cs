a1f273bbc90df5c966402ecc6e3c3f70d0f8cec2
rafetr <rafetr@risk.az>
2021-06-27 16:00:43 +0400

    lesson 20

---
+++ Lesson20/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Lesson20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Enum.GetName(WeekDays.Monday));
+
+            //foreach (var item in Enum.GetNames(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //foreach (var item in Enum.GetValues(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine((int)item);
+            //}
+
+            string m = "Monday1";
+
+            //WeekDays wd = (WeekDays)Enum.Parse(typeof(WeekDays), m);
+            //Console.WriteLine((int)wd);
+
+            //if(Enum.TryParse(typeof(WeekDays), m, out object wd))
+            //{
+            //    WeekDays days = (WeekDays)wd;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Can't convert");
+            //}
+
+            //string n = "123";
+
+            //if (int.TryParse(n, out int number))
+            //{
+            //    Console.WriteLine(number);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Can't convert");
+            //}
+
+            try
+            {
+                Console.WriteLine( ThrowExample.Sqrt(-1));
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.Error.WriteLine("Error: {0}", e);
+            }
+
+        }
+
+        enum WeekDays
+        {
+            Monday,
+            Thuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ Lesson20/ThrowExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson20
+{
+    class ThrowExample
+    {
+        public static double Sqrt(double value)
+        {
+            if (value < 0)
+                throw new System.ArgumentOutOfRangeException(
+                   "Sqrt for negative numbers is undefined!");
+            return Math.Sqrt(value);
+        }
+
+    }
+}
