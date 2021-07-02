1e46733ed1b5fa2353a63f370883c3f93fdf818d
rafetr <rafetr@risk.az>
2021-07-02 19:04:16 +0400

    lesson21

---
+++ Lesson21/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Questions questions = new Questions();
+            //questions.HediyyeMeselesi("12:13:14", "14:07:11");
+            //questions.HeddiyyeMeselesi2(new DateTime(1, 1, 1, 12, 13, 14), new DateTime(1, 1, 1, 14, 7, 11));
+            //questions.HediyyeMeselesi3(new TimeSpan(12, 13, 14), new TimeSpan(14, 7, 11));
+            //questions.SinusMeselesi(1, 3, 0.1);
+            //questions.SinusMeselesi2(30, 90, 5);
+            //questions.NaturalBolenler(100);
+            questions.Ebob(30000, 25);
+            questions.Ebob(12);
+        }
+    }
+}
---
+++ Lesson21/Questions.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21
+{
+    class Questions
+    {
+        public void HediyyeMeselesi(string time1, string time2)
+        {
+            var subtractSecond = GetSecond(time2) - GetSecond(time1);
+
+            Console.WriteLine(ConvertSecondToHMS(subtractSecond));
+        }
+
+        private int GetSecond(string time)
+        {
+            return int.Parse(time.Substring(0, 2)) * 3600 
+                +  int.Parse(time.Substring(3, 2)) * 60 
+                +  int.Parse(time.Substring(6));
+        }
+
+        private string ConvertSecondToHMS(int second)
+        {
+            return $"{second / 3600}:{(second % 3600) / 60}:{second % 60}";
+        }
+
+        public void HeddiyyeMeselesi2(DateTime time1, DateTime time2)
+        {
+            var x = time2 - time1;
+
+            Console.WriteLine(x.ToString());
+        }   
+        
+        public void HediyyeMeselesi3(TimeSpan time1,TimeSpan time2)
+        {
+            var x = time2 - time1;
+
+            Console.WriteLine(x.ToString());
+        }
+
+        public void SinusMeselesi(double a, double b, double h)
+        {
+            //rad format
+            for (double i = a; i <= b; i+=h)
+            {
+                Console.WriteLine($"{i}={3 * Math.Sin(i)}");
+            }
+        }
+
+        public void SinusMeselesi2(double a, double b, double h)
+        {
+            //degree format
+            for (double i = a; i <= b; i += h)
+            {
+                Console.WriteLine($"{i}={3 * Math.Sin(i * Math.PI / 180)}");
+            }
+        }
+
+        public void NaturalBolenler(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    Console.WriteLine($"{n}/{i}={n / i}");
+            }
+        }
+
+        public void Ebob(int a, int b)
+        {
+            for (int i = Math.Min(a,b); i>=1; i--)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+
+        public void Ebob(params int[] a)
+        {
+           //for Emmar
+        }
+    }
+}
