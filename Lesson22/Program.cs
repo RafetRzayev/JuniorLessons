7372fcfdb74296008f582ad550cbc947f0047565
rafetr <rafetr@risk.az>
2021-07-03 15:31:20 +0400

    lesson22

---
+++ Lesson22/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question question = new Question();
+            Console.WriteLine(question.Ebob(12,36,45));
+        }
+    }
+}
---
+++ Lesson22/Question.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson22
+{
+    class Question
+    {
+        public int Ebob(params int[] numbers)
+        {
+            for (int i = GetMin(numbers); i >= 1; i--)
+            {
+                if (CheckDivide(i, numbers))
+                {
+                    return i;
+                }
+            }
+
+            return 1;
+        }
+
+        private bool CheckDivide(int n, params int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % n != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private int GetMin(params int[] numbers)
+        {
+            int min = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (min > numbers[i])
+                    min = numbers[i];
+            }
+
+            return min;
+        }
+    }
+}
